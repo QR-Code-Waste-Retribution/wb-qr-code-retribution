a956f66683d5d1794b532f5671a00dfb67b809a1
andreaszico <zicoaritonang@gmail.com>
2023-05-13 10:24:55 +0700

    fixing

--- user.js
+++ user.js
@@ -1,13 +1,10 @@
-const redis = require("redis");
-const client = redis.createClient();
+let users = [];
 
 exports.addUser = ({ id, name, room }) => {
   if (!name || !room) return { error: "name and room required." };
-
-  // Store user details in Redis
   const user = { id, name, room };
-  client.sAdd(`user:${id}`, user);
-  client.sAdd(`room:${room}:users`, id);
+
+  users.push(user);
 
   return { user };
 };
